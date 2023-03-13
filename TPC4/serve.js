cca669ad8711e447a2033262e57d8deffa2beee0
SugaryLump <sugarcanegaming6000@gmail.com>
2023-03-13 01:30:21 +0000

    Finished homework 4

--- TPC4/serve.js
+++ TPC4/serve.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 
-exports.mainPage = (res, tasks) => {
+exports.mainPage = (res, tasks, edit) => {
     fs.readFile("public/toDo.html", (err, data) => {
         if(err){
             console.log('File not found: ' + err)
@@ -8,28 +8,115 @@ exports.mainPage = (res, tasks) => {
             res.end('File not found: ' + err)
         }
         else {
-            var taskFormString = `<form id="task-form" method="post">
-    <p class="small-text">Description</p>
-    <div 
-        contenteditable
-        placeholder="Description"
-        class="text-block input"
-        id="text-form-description">
+            if (edit == null) {
+                var taskFormString = `
+    <form id="task-form" method="post">
+        <p class="small-text">Description</p>
+        <div 
+            contenteditable
+            placeholder="Description"
+            class="text-block input"
+            id="text-form-description"></div>
+        <input id="description-hidden" type="hidden" name="description">
+        <input type="hidden" name="completed" value=false>
+        <div id="task-form-field-row">
+            <input class="input" placeholder="Assigned To" name="assigned">
+            <input class="input" type=date placeholder="Due Date" name="date">
+        </div>
+        <button class="add-button" type="submit" onclick="getDivValue()">Add</button>
+        <script>
+            function getDivValue() {
+                document.getElementById("description-hidden").value = document.getElementById("text-form-description").innerHTML;
+                console.log("a" + document.getElementById("text-form-description").innerHTML)
+            }
+        </script>
+    </form>
+`
+            }
+            else {
+                console.log("Doing editing routine")
+                var taskFormString = `
+    <form id="task-form" method="post">
+        <p class="small-text">Description</p>
+        <div 
+            contenteditable
+            placeholder="Description"
+            class="text-block input"
+            id="text-form-description">${edit.description}</div>
+        <input id="description-hidden" type="hidden" name="description">
+        <input type="hidden" name="completed" value=${edit.completed}>
+        <input type="hidden" name="id" value=${edit.id}>
+        <div id="task-form-field-row">
+            <input class="input" placeholder="Assigned To" name="assigned" value="${edit.assigned}">
+            <input class="input" type=date placeholder="Due Date" name="date" value="${edit.date}">
+        </div>
+        <div id="edit-button-row">
+            <a class="add-button" href="/">Cancel</a>
+            <button class="add-button" type="submit" onclick="getDivValue()">Edit</button>
+            <script>
+                function getDivValue() {
+                    document.getElementById("description-hidden").value = document.getElementById("text-form-description").innerHTML;
+                    console.log("a" + document.getElementById("text-form-description").innerHTML)
+                }
+            </script>
+        </div>
+    </form>
+`
+            }
+            var pendingTasksString = ""
+            tasks.forEach(task => {
+                if (task.completed == "false") {
+                    pendingTasksString += `
+    <div class="task-card">
+        <div class="task-card-button-row">
+            <form method="get">
+                <input type="hidden" name="_method" value="COMPLETE">
+                <input type="hidden" name="id" value="${task.id}">
+                <button class="icon-button" type="submit"><i class="fas fa-check icon-button green-hover"></i></button>
+            </form>
+            <a href="/${task.id}"><i class="fa fa-file icon-button yellow-hover"></i></a>
+            <form method="get">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="${task.id}">
+                <button class="icon-button" type="submit"><i class="fas fa-times icon-button red-hover"></i></i></button>
+            </form>
+        </div>
+        <p>${task.description}</p>
+        <div class="task-card-field-row">
+            <p><b>${task.assigned}</b></p>
+            <p><b>${task.date}</b></p>
+        </div>
     </div>
-    <input type="hidden" name="description">
-    <div id="task-form-field-row">
-        <input class="input" placeholder="Assigned To" name="assigned">
-        <input class="input" type=date placeholder="Due Date" name="date">
-    </div>
-    <button class="add-button" type="submit" onclick="getDivValue()">Add</button>
-    <script>
-        function getDivValue() {
-            document.getElementById("description-hidden").value = document.getElementById("text-form-description").innerHTML;
-            console.log("a" + document.getElementById("text-form-description").innerHTML)
-        }
-    </script>
-</form>`
-            //var pendingTasksString = `<form id="task-form`
+`
+            }})
+
+            var completeTasksString = ""
+            tasks.forEach(task => {
+                if (task.completed == "true") {
+                    completeTasksString += `
+    <div class="task-card-done">
+        <div class="task-card-button-row">
+            <form method="get">
+                <input type="hidden" name="_method" value="UNDO">
+                <input type="hidden" name="id" value="${task.id}">
+                <button class="icon-button" type="submit"><i class="fas fa-undo icon-button yellow-hover"></i></button>
+            </form>
+            <a href="/${task.id}"><i class="fa fa-file icon-button yellow-hover"></i></a>
+            <form method="get">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="${task.id}">
+                <button class="icon-button" type="submit"><i class="fas fa-times icon-button red-hover"></i></i></button>
+            </form>
+        </div>
+        <p>${task.description}</p>
+        <div class="task-card-field-row">
+            <p><b>${task.assigned}</b></p>
+            <p><b>${task.date}</b></p>
+        </div>
+    </div>                
+`
+            }})
+
             console.log("Serving toDo.html")
             res.writeHead(200, {"Content-Type": 'text/html charset=utf-8'})
             res.end(eval('`' + data + '`'))
--- TPC4/toDo.js
+++ TPC4/toDo.js
@@ -3,11 +3,27 @@ var url = require('url')
 var serve = require('./serve.js')
 var static = require('./static.js')
 var axios = require("axios")
+const { parse } = require('querystring')
 
 var port = 7777
 var dbURL = "http://localhost:3000/tasks"
 
 var tasks = { }
+
+function collectRequestBodyData(request, callback) {
+    if(request.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
 
 http.createServer((req, res) => {
     var parsedURL = url.parse(req.url, true);
@@ -18,15 +34,97 @@ http.createServer((req, res) => {
     else {
         console.log(`Method is ${req.method}`)
         switch (req.method) {
-            default: {
-                console.log(String(req))
+            case "POST": {
                 axios.get(dbURL)
                 .then(axiosRes => {
-                    serve.mainPage(res, axiosRes.data)
+                    collectRequestBodyData(req, task => {
+                        if (parseInt(parsedURL.pathname.slice(1)) != NaN && parseInt(parsedURL.pathname.slice(1)) <= axiosRes.data.length) {
+                            axios.put(dbURL + `/${parseInt(parsedURL.pathname.slice(1))}`, task)
+                            .then(axiosPutRes => {
+                                console.log("Put response: " + axiosPutRes.status)
+                                res.writeHead(302, {'Location':'/'})
+                                res.end()
+                            })
+                            .catch(axiosPutErr => {
+                                console.log(`Axios PUT error: ${axiosPutErr}`)
+                            })
+                        }
+                        else {
+                            axios.post(dbURL, task, {headers: {'Content-Type': 'application/json'}})
+                            .then(axiosPostRes => {
+                                console.log("Post response: " + axiosPostRes.status)
+                                res.writeHead(302, {'Location':'/'})
+                                res.end()
+                            })
+                            .catch(axiosPostErr => {
+                                console.log(`Axios POST error: ${axiosPostErr}`)
+                            })
+                        }
+                    })
                 })
                 .catch(axiosErr => {
                     console.log(`Axios error: ${axiosErr}`)
-                    res.end(String(axiosErr))
+                })
+                break;
+            }
+            default: {
+                axios.get(dbURL)
+                .then(axiosRes => {
+                    var edit = null;
+                    if (parseInt(parsedURL.pathname.slice(1)) != NaN && parseInt(parsedURL.pathname.slice(1)) <= axiosRes.data.length) {
+                        edit = axiosRes.data.find(task => task.id == parsedURL.pathname.slice(1))
+                        console.dir(edit)
+                    }
+
+                    switch (parsedURL.query._method) {
+                        case "COMPLETE": {
+                            var task = axiosRes.data.find(task => task.id == parsedURL.query.id)
+                            task.completed = "true"
+                            axios.put(dbURL + `/${parsedURL.query.id}`, task)
+                            .then(axiosPutRes => {
+                                console.log(`Completed task ${parsedURL.query.id}`)
+                                res.writeHead(302, {'Location':'/'})
+                                    res.end()
+                            })
+                            .catch(axiosPutErr => {
+                                console.log(`Axios DELETE error: ${axiosPutErr}`)
+                            })
+                            break
+                        }
+                        case "DELETE": {
+                            axios.delete(dbURL + `/${parsedURL.query.id}`, )
+                            .then(axiosDeleteRes => {
+                                console.log(`Deleted task ${parsedURL.query.id}`)
+                                res.writeHead(302, {'Location':'/'})
+                                res.end()
+                            })
+                            .catch(axiosDeleteErr => {
+                                console.log(`Axios DELETE error: ${axiosDeleteErr}`)
+                            })
+                            break
+                        }
+                        case "UNDO": {
+                            var task = axiosRes.data.find(task => task.id == parsedURL.query.id)
+                            task.completed = "false"
+                            axios.put(dbURL + `/${parsedURL.query.id}`, task)
+                            .then(axiosPutRes => {
+                                console.log(`Undid task ${parsedURL.query.id}`)
+                                axios.get(dbURL)
+                                res.writeHead(302, {'Location':'/'})
+                                res.end()
+                            })
+                            .catch(axiosPutErr => {
+                                console.log(`Axios DELETE error: ${axiosPutErr}`)
+                            })
+                            break
+                        }
+                        default: {
+                            serve.mainPage(res, axiosRes.data, edit)
+                        }
+                    }
+                })
+                .catch(axiosErr => {
+                    console.log(`Axios GET error 1: ${axiosErr}`)
                 })
             }
         }
